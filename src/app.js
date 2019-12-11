2a8c1ac8ecb7726a75cd41147affb246d573317a
Ridhwan Adagiri <ibrahimridwan47@gmail.com>
2019-12-10 19:47:36 -1100

    Service Worker, offline caching reload configured

--- src/app.js
+++ src/app.js
@@ -2,19 +2,28 @@ import React from "react";
 import ReactDom from "react-dom";
 import configuration from "./store/configuration";
 import MainRouter from "./router/MainRouter";
-import 'react-dates/lib/css/_datepicker.css';
-import 'react-dates/initialize';
+import "react-dates/lib/css/_datepicker.css";
+import "react-dates/initialize";
 import "./store/configuration";
 import { Provider } from "react-redux";
 import "./styles/styles.scss";
 
+//serviceWorkers
 
-const store = configuration()
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("serviceWorker.js")
+      .then(reg => console.log("Service Workers"))
+      .catch(err => console.log(`Service Worker: Error: ${err}`));
+  });
+}
 
+const store = configuration();
 
-
-
-const jsx = (<Provider store = {store}>
-<MainRouter />
-</Provider>);
+const jsx = (
+  <Provider store={store}>
+    <MainRouter />
+  </Provider>
+);
 ReactDom.render(jsx, document.getElementById("app"));
